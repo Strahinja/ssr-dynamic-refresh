3c46c90d42e53365801a336444e64147ba28d4c6
Страхиња Радић <Strahinja@users.noreply.github.com>
2019-11-04 06:57:58 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <nuxt-link to="/search/first">Search: "first"</nuxt-link>
+    <nuxt-link to="/search/second">Search: "second"</nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Index"
+};
+</script>
+
+<style scoped>
+html,
+body {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-weight: 400;
+  font-size: 11pt;
+}
+</style>
---
+++ pages/search/_docid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <nuxt-link to="/">Back to home</nuxt-link>
+    <p>{{docBodies[documentId]}}</p>
+  </div>
+  <!--doc-->
+</template>
+
+<script>
+export default {
+  name: "Search",
+  asyncData({ params }) {
+    return {
+      documentId: params.docid,
+      docBodies: {
+        first: "First document",
+        second: "Second document"
+      }
+    };
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
